5333c345fc0b4a613b51c930457e8f7218c0b4be
dudgns1170 <80243569+dudgns1170@users.noreply.github.com>
2021-05-24 12:45:09 +0900

    5/24 SQL

--- out_user.sql
+++ out_user.sql
@@ -198,3 +198,157 @@ FROM emp_dept_v1;
 drop view emp_dept_v1;
 
 --인덱스 
+                           
+                           --5/24 수업
+
+--인덱스 
+--create unique index ex2_10_ix01
+--on ex2_10(col11);
+
+
+--시노님(동의어) 생성, (채널 테이블으로 만듬.)
+create or replace synonym syn_channe1
+for channels;
+
+SELECT COUNT(*)
+    FROM syn_channe1;
+
+--다른 계정으로 로그인하여참조 해보기
+
+alter user hr IDENTIFIED by hr ACCOUNT UNLOCK;
+
+--hr사용자로 로그인후 조회
+--실행명령어는  grant 권한부여 후 동일 
+--out_user 을 사용하여 접근해야함.
+
+select COUNT(*)
+from out_user.syn_channe1;
+
+--오류 발생 시 grant 권한 부여
+
+--grant select on syn_channel to hr;
+
+--public 나노님 생성(생성후 조희시 out_user. 작성하지 않아도 된다.)
+
+create or replace public synonym syn_channel2
+for channels;
+
+--조회 권한을 public에 전달
+
+grant select on syn_channel2 to public;
+
+--hr로 로그인 후 조회
+
+select count(*)
+from syn_channel2;
+
+--synonym 삭제
+
+drop synonym syn_channe1;
+
+--public synonym 삭제
+drop public synonym syn_channel2;
+
+--시퀀스 생성
+
+create SEQUENCE my_seq1  
+increment by 1
+start with 1
+minvalue 1
+maxvalue 1000
+nocycle
+nocache;
+
+--기존 데이터 삭제
+delete ex2_8; 
+--삭세 후  my.seq1 데이터 삽입
+insert into ex2_8 (col1) values (my_seq1.nextval);
+insert into ex2_8 (col1)values (my_seq1.nextval);
+
+select *
+from ex2_8;
+
+--시퀀스명.currval 현재 값 확인 
+select my_seq1.currval
+from dual;
+
+--nextval (다음숫자로 넘길시)값 가져오기
+insert into ex2_8 (col1)values (my_seq1.nextval);
+
+select *
+from ex2_8;
+
+--SELECT 문을 사용하여 값 증가하기
+select my_seq1.nextval
+from dual;
+
+insert into ex2_8 (col1)values (my_seq1.nextval);
+
+select*
+from ex2_8;
+
+--시퀸스 삭제
+
+drop sequence my_seq1;
+
+
+--ex1)
+-- 테이블 생성
+--ORDER_ID 기본키
+--ORDER_MODE ,ORDER_TOTAL 제약
+
+CREATE TABLE ORDESR(
+ORDER_ID number(12,0) PRIMARY KEY,
+ORDER_DATE DATE,
+ORDER_MODE VARCHAR2(8 BYTE)CONSTRAINT ck_ORDER1 check(ORDER_MODE in ('dirrct', 'online')),
+CUSTOMER_ID NUMBER(6,0),
+ORDER_STATUS NUMBER(2,0),
+ORDER_TOTAL  NUMBER(8,2) Default 0,
+SALES_REP_ID NUMBER(6,0),
+PROMOTION_ID NUMBER(6,0)
+);
+
+--제약조건
+
+drop table ORDESR;
+
+--ex2
+CREATE TABLE ORDER_ITEMS(
+ORDER_ID NUMBER(12,0) ,
+LINE_ITEM_ID NUMBER(3,0),
+CONSTRAINT PK_ID PRIMARY KEY( ORDER_ID, LINE_ITEM_ID),
+PRODUCT_ID NUMBER(3,0),
+UNIT_PRICE NUMBER(8,2) DEFAULT 0,
+QUANTITY NUMBER(8,0) DEFAULT 0
+);
+
+drop table ORDER_ITEMS;
+
+--ex3)
+CREATE TABLE PROMTIONS(
+PROMO_ID NUMBER(6,0) PRIMARY KEY,
+PROMO_NAME VARCHAR(20)
+);
+--ex4
+
+--ex5
+create SEQUENCE ORDERA_SEQ  
+increment by 1
+start with 1000
+minvalue 1
+maxvalue 99999999
+nocycle
+nocache;
+
+
+---SELECT문
+-- 원하는 정보를 조회
+
+SELECT employee_id, emp_name
+from  employees
+where  salary> 5000
+AND job_id = ' IT_PROG'
+--사번으로 정렬
+ORDER BY employee_id;
+--조건을 통한 검색
+
