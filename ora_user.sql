bf6ef9561d05145189e4c37e7f5d8a8016b64c6c
dudgns1170 <80243569+dudgns1170@users.noreply.github.com>
2021-05-27 20:14:57 +0900

    5/27 SQL

--- ora_user.sql
+++ ora_user.sql
@@ -1,346 +1,3 @@
-/*새로운 태이블 생성*/
-CREATE TABLE ex2_2(
-/*테이블 컬럼 데이터 정의*/
-column1 VARCHAR2(3),
-column2 VARCHAR2(3 BYTE),
-column3 VARCHAR2(3 CHAR)
-);
-
-INSERT INTO ex2_2 VALUES('abc','abc','abc');
-/*문자열 길이 확인 하는 문구*/
-SELECT column1, length(column1) as len1,
-    column2, length(column2) as len2,
-    column3, length(column3) as len3
-FROM ex2_2;
-
-/*한글은 한글자 당 2byte로 형성되어 있어 오류 발생 */
-/*INSERT INTO ex2_2 VALUES('홍길동','홍길동','홍길동');*/
-
-INSERT into ex2_2(column3) VALUES('홍길동');
-
-SELECT column3, length(column3) as len3, LENGTHB(column3) as bytelen
-FROM ex2_2;
-
-CREATE TABLE ex2_3(
-COL_INT INTEGER,
-COL_DEC DECIMAL,
-COL_NUM NUMBER
-);
-
-SELECT column_id, column_name, data_type, data_length
-    FROM user_tab_cols
-    WHERE table_name = 'EX2_3'
-    ORDER BY column_id;
-
-CREATE TABLE EX2_4(
-    COL_FLOT1 FLOAT(32),
-    COL_FLOT2 FLOAT
-);
-
-INSERT INTO ex2_4 (col_flot1, col_flot2) VALUES (1234567891234, 1234567891234);
-
-CREATE TABLE EX2_5(
-    COL_DATE DATE,
-    COL_TIMESTAMP TIMESTAMP
-);
-
-INSERT INTO EX2_5 VALUES (SYSDATE, SYSTIMESTAMP);
-SELECT *
-FROM EX2_5;
-
-
-CREATE TABLE EX2_6(
-    COL_NULL VARCHAR2(10),
-    COL_NOT_NULL VARCHAR2(10) NOT NULL
-);
-
-INSERT INTO EX2_6 VALUES ('AA','BB');
-
-SELECT constraint_name, constraint_type, table_name, search_condition
-FROM user_constraints
-WHERE table_name = 'EX2_6';
-
-CREATE TABLE EX2_7(
-COL_UNIQUE_NULL VARCHAR2(10) UNIQUE,
-COL_UNIQUE_NNULL VARCHAR2(10) UNIQUE NOT NULL,
-COL_UNIQUE VARCHAR2(10),
-CONSTRAINTS unique_nm1 UNIQUE (COL_UNIQUE)
-);
-
-SELECT constraint_name, constraint_type, table_name, search_condition FROM user_constraints WHERE table_name = 'EX2_7';
-
-INSERT INTO EX2_7 VALUES ('AA', 'AA', 'AA');
-INSERT INTO EX2_7 VALUES ('AA', 'AA', 'AA');
-
-INSERT INTO EX2_7 VALUES ('', 'BB', 'BB');
-INSERT INTO EX2_7 VALUES ('', 'CC', 'CC');
-
-
-CREATE TABLE EX2_8(
-    COL1 VARCHAR2(10) PRIMARY KEY,
-    COL2 VARCHAR2(10)
-);
-
-SELECT constraint_name, constraint_type, table_name, search_condition
-FROM user_constraints
-WHERE table_name = 'EX2_8';
-
---INSERT INTO EX2_8 VALUES ('','AA'); NULL 입력 불가
-
-INSERT INTO EX2_8 VALUES ('AA','AA');
-
---INSERT INTO EX2_8 VALUES ('AA','AA'); 무결성 제약 조건 위배
-
-CREATE TABLE ex2_9(
-num1 NUMBER
-CONSTRAINTS check11 check (num1 BETWEEN 1  and 9),
-gender VARCHAR2(10)
-CONSTRAINTS check22 check ( gender in ('MAKES','FEMALES'))
-);
-
-drop table ex2_91;
-
-SELECT constraint_name, constraint_type, table_name , search_condition
-from user_constraints
-WHERE table_name='EX2_9';
-
--- check1은 1부터 9까지 제약을 두고 있으며 ,check2는 make,female 값만 들어갈수 있도록
---설정되어 출력되지 않는다
---insert into EX2_9 VALUES(10,'man')
-
-insert into EX2_9 VALUES (5,'FEMALES');
-
-
-CREATE TABLE  ex2_10 (
-col1 varchar2(10) not null,
-col2 VARCHAR2(10) null,
-Create_date DATE DEFAULT SYSDATE);
-
---5/18일 오라클 
-
---행에 데이터  삽입
-INSERT into ex2_10(col1, col2) VALUES('AA','BB');
-
--- 테이블 데이터 조회 
-SELECT *
-FROM ex2_10;
-
--- 테이블 삭제
---DROP TABLE ex2_10 (생략 가능 /CASCADE CONSTRAINTS);
-
-
--- 테이블 변경 명령어  
---ALTER TABLE명령어로 변경 
-
---컬럼명 이름 변경하기
-ALTER TABLE ex2_10 RENAME COLUMN cloll to COL11;
-
-DESC ex2_10;
-
---컬럼 타입 변경 하기
-ALTER TABlE ex2_10 MODIFY col2 VARCHAR2(30);
-
-DESC ex2_10;
-
---컬럼  및 타입 추가 명령어
-ALTER TABle ex2_10 add col3 NUMBER;
-
-DESC ex2_10;
-
---특정 컬럼 삭제하기
-ALTER TABLE ex2_10 DROP COLUMN col3;
-
-DESC ex2_10;
-
--- 제약조건추가 /기본키 추가
-
-ALTER TABLE ex2_10 ADD CONSTRAINTS pk_ex2_10 PRIMARY KEY (col11);
-
-SELECT constraint_name,constraint_type, table_name, search_condition
-FROM user_constraints
-where table_name = 'EX2_10';
-
---제약 조건 삭제/ 기본키 삭제
-ALTER TABLE ex2_10 DROP CONSTRAINT pk_ex2_10;
-
-SELECT constraint_name,constraint_type, table_name, search_condition
-FROM user_constraints
-where table_name = 'EX2_10';
-
---테이블 복사
-
-CREATE TABLE  ex2_9_1 AS
-SELECT *
-from ex2_9;
-
---테이블 삭제
-DROP TABLE ex2_9_1;
-
-
---5/21일 수업
---뷰 생성하기
-
-CREATE OR REPLACE VIEW  emp_dept_v1 AS
--- 뷰 테이블 생성 명렁어 
-
-SELECT a.employee_id, a.emp_name, a.department_id, b.department_name  --부서명 컬럼
-FROM employees a, departments b  
-WHERE a.department_id = b.department_id;
-
-
--- 뷰 실헹
-SELECT
-    *
-FROM emp_dept_v1;
-
---뷰 삭제
-
-drop view emp_dept_v1;
-
---인덱스 
-                           
-                           --5/24 수업
-
---인덱스 
---create unique index ex2_10_ix01
---on ex2_10(col11);
-
-
---시노님(동의어) 생성, (채널 테이블으로 만듬.)
-create or replace synonym syn_channe1
-for channels;
-
-SELECT COUNT(*)
-    FROM syn_channe1;
-
---다른 계정으로 로그인하여참조 해보기
-
-alter user hr IDENTIFIED by hr ACCOUNT UNLOCK;
-
---hr사용자로 로그인후 조회
---실행명령어는  grant 권한부여 후 동일 
---out_user 을 사용하여 접근해야함.
-
-select COUNT(*)
-from out_user.syn_channe1;
-
---오류 발생 시 grant 권한 부여
-
---grant select on syn_channel to hr;
-
---public 나노님 생성(생성후 조희시 out_user. 작성하지 않아도 된다.)
-
-create or replace public synonym syn_channel2
-for channels;
-
---조회 권한을 public에 전달
-
-grant select on syn_channel2 to public;
-
---hr로 로그인 후 조회
-
-select count(*)
-from syn_channel2;
-
---synonym 삭제
-
-drop synonym syn_channe1;
-
---public synonym 삭제
-drop public synonym syn_channel2;
-
---시퀀스 생성
-
-create SEQUENCE my_seq1  
-increment by 1
-start with 1
-minvalue 1
-maxvalue 1000
-nocycle
-nocache;
-
---기존 데이터 삭제
-delete ex2_8; 
---삭세 후  my.seq1 데이터 삽입
-insert into ex2_8 (col1) values (my_seq1.nextval);
-insert into ex2_8 (col1)values (my_seq1.nextval);
-
-select *
-from ex2_8;
-
---시퀀스명.currval 현재 값 확인 
-select my_seq1.currval
-from dual;
-
---nextval (다음숫자로 넘길시)값 가져오기
-insert into ex2_8 (col1)values (my_seq1.nextval);
-
-select *
-from ex2_8;
-
---SELECT 문을 사용하여 값 증가하기
-select my_seq1.nextval
-from dual;
-
-insert into ex2_8 (col1)values (my_seq1.nextval);
-
-select*
-from ex2_8;
-
---시퀸스 삭제
-
-drop sequence my_seq1;
-
-
---ex1)
--- 테이블 생성
---ORDER_ID 기본키
---ORDER_MODE ,ORDER_TOTAL 제약
-
-CREATE TABLE ORDESR(
-ORDER_ID number(12,0) PRIMARY KEY,
-ORDER_DATE DATE,
-ORDER_MODE VARCHAR2(8 BYTE)CONSTRAINT ck_ORDER1 check(ORDER_MODE in ('dirrct', 'online')),
-CUSTOMER_ID NUMBER(6,0),
-ORDER_STATUS NUMBER(2,0),
-ORDER_TOTAL  NUMBER(8,2) Default 0,
-SALES_REP_ID NUMBER(6,0),
-PROMOTION_ID NUMBER(6,0)
-);
-
---제약조건
-
-drop table ORDESR;
-
---ex2
-CREATE TABLE ORDER_ITEMS(
-ORDER_ID NUMBER(12,0) ,
-LINE_ITEM_ID NUMBER(3,0),
-CONSTRAINT PK_ID PRIMARY KEY( ORDER_ID, LINE_ITEM_ID),
-PRODUCT_ID NUMBER(3,0),
-UNIT_PRICE NUMBER(8,2) DEFAULT 0,
-QUANTITY NUMBER(8,0) DEFAULT 0
-);
-
-drop table ORDER_ITEMS;
-
---ex3)
-CREATE TABLE PROMTIONS(
-PROMO_ID NUMBER(6,0) PRIMARY KEY,
-PROMO_NAME VARCHAR(20)
-);
---ex4
-
---ex5
-create SEQUENCE ORDERA_SEQ  
-increment by 1
-start with 1000
-minvalue 1
-maxvalue 99999999
-nocycle
-nocache;
-
-
 ---SELECT문
 -- 원하는 정보를 조회
 
@@ -742,3 +399,267 @@ WHERE  salary >=2000 and salary <= 2500
 ORDER BY employee_id;
 
 --5) p123
+
+---5/27 수업
+--함수
+
+--숫자함수
+-- ABS() - 절대값 반환 함수
+SELECT ABS(10), ABS(-10), ABS(-10.234)
+FROM DUAL;
+-- CEIL(N) - n과 같거나 가장 큰 정수 반환
+SELECT  CEIL(10.123), CEIL(10.541), CEIL(11.001)
+FROM DUAL;
+-- FLOOR(n) - n보다 작거나 가장 작은 정수 반환
+SELECT FLOOR(10.123), FLOOR(10.541), FLOOR(11.001)
+FROM DUAL;
+-- ROUND( n , i) - n을 소수점 기준(n+1)번째에서 반올림한 결과 반환9 ( 소수점 두번째까지 보여준다)
+SELECT ROUND(10.123), ROUND(10.541), ROUND(11.001)
+FROM DUAL;
+SELECT ROUND(10.123,1), ROUND(10.541,2), ROUND(10.541,3)
+FROM DUAL;
+SELECT ROUND(0,3) , ROUND(115.155,-1), ROUND(115.155, -2)
+FROM DUAL;
+--TRUNC(n1,n2) - n1을 소수점 기준 n2자리에서 무조건 잘라낸 결과 반환 
+SELECT TRUNC(115.155), TRUNC(115.155,1), TRUNC(115.155,2), TRUNC(115.155,-2)
+FROM DUAL;
+
+--POWER(n2,n1) - n2를 n1 제곱한 결과를 반환 n2가 음수이면 n1은 반드시 정수
+SELECT POWER(3,2), POWER(3,3), POWER(3,3.0001)
+FROM DUAL;
+
+--SQRT(n)- n의 제곱근 반환
+SELECT SQRT(2), SQRT(5)
+FROM DUAL;
+
+--MOD(n2, n1) - n2를 n1으로 나눈 남너지 값을 반환
+SELECT MOD(19,4), MOD(19.123, 4.2)
+FROM DUAL;
+
+SELECT REMAINDER(19,4),  REMAINDER(19.123, 4.2)
+FROM DUAL;
+
+SELECT EXP(2), LN(2.713), LOG(10,100)
+FROM DUAL;
+
+-- 문자함수
+INITCAP -char의 첫 문자를 대문자로 변경 인식 기준 공백
+SELECT INITCAP('naver say goodbay')
+FROM DUAL;
+
+LOWER - 소문자 변환
+SELECT LOWER('NAVER SAY GOODBEY')
+FROM DAUL;
+
+UPPER - 대문자 변환 후 반환
+SELECT UPPER('say goodbye')
+FROM DAUL;
+
+CONCAT- 두문자를 연결
+SELECT CONCAT('I HAVE' , 'good boy') , 'I HAVE ' | | 'Good boy'
+FROM DUAL;
+
+SUBSTR (CHAR, POS, len)-  len길이만큼 잘라낸 결과
+SELECT SUBSTR ('ABCDEF' 1,4),  SUBSTR('ABCDEFG' ,-1,4)
+FROM DUAL;
+
+SUBSTRB (CHAR, POS, len)-  바이트 수 단위로 반환
+SELECT SUBSTRB ('ABCDEF' 1,4) , SUBSTRB('가나다라마바사' ,-1,4)
+FROM DUAL;
+
+
+LTRIM(CHAR,SET) - char 에서 set으로 지정된 문자열을 왼쪽끝에서 제거 후 나머지 문자여 
+RTRIM(CHAR,SET)- LTRIM 과 반대로 오른쪽 끝에서 제거 후 나머지 문자열 반환
+SELECT LTRIM('ABCDEFG', 'ABC'),
+        LTRIM('가나다라','가'),
+        RTRIM('ABCDEFG', 'ABC'),
+        RTRIM ('가나다라','라')
+FROM DUAL;
+
+LAPD(expr1,expr2) - 뒷 문자열을 n자리 만큼 왼쪽부터 채워서 반환
+
+CREATE TABLE ex4_1(
+phone_num VARCHAR2(30)
+);
+INSERT INTO ex4_1 VALUES('111-3333');
+--                                    12는 총자리수를 의미한다.
+SELECT LPAD(phone_num , 12,'(02)')
+FROM ex4_1;
+
+RPAD  -LAPD 반대 오른쪽
+SELECT RPAD(phone_num , 12,'(02)')
+FROM ex4_1;
+
+REPLACE- char에서 변환
+SELECT REPLACE('나는 너를', '나', '너')
+FROM DUAL;
+
+TRANSLATE - 한글자씩 바꾼 결과
+SELECT REPLACE('나는 너를', '나는', '너를') AS rep,
+        TRANSLATE('나는 너를' ,'나는', '너를' ) AS trn
+FROM DUAL;
+
+
+--INSTR- 
+SELECT INSTR('내가 만약 외로울 떄면,내가 만약 외로울 떄면,내가 만약 외로울 떄면','만약') AS INSTR1,
+INSTR('내가 만약 외로울 떄면, 내가 만약 외로울 떄면, 내가 만약 외로울 떄면'  ,'만약',5) AS INSTR2,
+INSTR('내가 만약 외로울 떄면, 내가 만약 외로울 떄면, 내가 만약 외로울 떄면' , '만약',5,2) AS INSTR3
+FROM DUAL;
+
+
+LENGTH - 길이반환
+LENGTHB -문자열 BYTE수 반환
+
+날짜 함수
+--SYSDATE,                    SYSTIMESTAMP
+--현재 일자와 시간 타입     일자 및 시간 및 초 
+SELECT SYSDATE, SYSTIMESTAMP
+FROM DUAL;
+
+--ADD_MONTH0S 월을 더한 값 ,마이너스한 값
+SELECT ADD_MONTHS(SYSDATE, 1) , ADD_MONTHS(SYSDATE , -1)
+FROM DUAL;
+
+--MONTHS_BETWEEN
+--두 날짜 사이의 개왈 수 반환
+SELECT MONTHS_BETWEEN(SYSDATE, ADD_MONTHS(SYSDATE,1)) mon1,
+        MONTHS_BETWEEN(ADD_MONTHS(SYSDATE,1),SYSDATE) mon2
+FROM DUAL;
+
+--LAST_DAY  해당월 의 마지막 일자 반환
+SELECT LAST_DAY(TO_DATE(20240202))
+FROM DUAL;
+
+ROUND-반올림한 날짜
+TRUNC - 잘라낸 날짜 반환
+
+SELECT SYSDATE, ROUND(SYSDATE, 'month'), TRUNC(SYSDATE,'month')
+FROM DUAL;
+
+NEXT_DAY 명시한 날짜로 다음주 주중 일자를 반환
+SELECT NEXT_DAY(SYSDATE, '금요일')
+FROM DUAL;
+
+--변환 함수
+
+TO_CHAR (숫자 혹은 날짜) FORMAT 에 맞게 변환 후결과반환
+SELECT TO_CHAR(123456789, '000,000,000')
+FROM DUAL;
+
+SELECT TO_CHAR(SYSDATE, 'YY-MM-DD-DL')
+FROM DUAL;
+
+TO_NUMBER -문자나 다른 유형의 숫자를 number 형으로 변환
+SELECT TO_NUMBER('12345678')
+FROM  dual;
+
+TO_DATE -날짜형으로 변환
+SELECT TO_DATE('20210527', 'YYYY-MM-DD')
+FROM DUAL;
+SELECT TO_DATE('2014010113:44:50', 'YYYY-MM-DDHH24:MI:SS')
+FROM DUAL;
+
+TO_TIMESTAMP -TIMESTAMP 형으로 변환
+SELECT TO_TIMESTAMP('2014010113:44:50', 'YYYY-MM-DDHH24:MI:SS')
+FROM DUAL;
+
+NULL 함수
+NVL expr1이 null일때 expr2를반환
+SELECT NVL(manager_id, employee_id)
+FROM employees
+WHERE manager_id IS NULL;
+
+NVL2  expr1이 null이 아니면 expr2, NULL이면 expr3 반환
+SELECT employee_id,
+        NVL2(commission_pct, salary +(salary *commission_pct), salary) AS salary2
+--커미션이  nuLL인 사원 그냥 급여를       null이 아니면                        컬럼에 표시
+FROM employees;
+
+--COALESCE- null이 아닌 첫번쨰 표현식을 반환,
+SELECT employee_id, salary, commission_pct,
+            COALESCE ( salary * commission_pct, salary) AS salary2
+FROM employees;
+
+--GREATEST-표현식에서 가장 큰 값을 반환
+
+LEAST- 표현식에서 가장 작은 값 반환
+
+
+
+
+--1
+CREATE TABLE PRODUCT(
+PRODUCT_ID    NUMBER(12,0)  PRIMARY KEY,
+PRODUCT_NAME   VARCHAR2(8 BYTE),
+QUANTITY   NUMBER(10,0) DEFAULT 0,
+ORDER_DATE DATE,
+ORDER_MODE   VARCHAR2(8 BYTE)
+CONSTRAINT ORDER1 check (ORDER_MODE in ('dirrct', 'online')),
+DESCRIPTION   VARCHAR2(20 BYTE),
+STANDARD_COST  NUMBER(4,0),
+LIST_PRICE   NUMBER(8,2) DEFAULT 0,
+CATEGORY_ID   NUMBER(6,0),
+PROMOTION_ID   NUMBER(6,0)
+);
+DROP TABLE PRODUCT;
+
+DROP TABLE EXAM2_2;;
+
+CREATE TABLE EXAM2_1(
+employee_id  NUMBER(6),
+emp_name  VARCHAR2(80),
+salary  NUMBER(8,2),
+manager_id NUMBER(6)
+);
+
+INSERT INTO EXAM2_1
+SELECT employee_id, emp_name, salary,  manager_id
+FROM employees
+WHERE manager_id = '147' AND salary BETWEEN '6000' AND '7000'
+ORDER BY employee_id;
+
+SELECT *
+FROM exam2_1;
+DELETE TABLE  EXAM2_1;
+
+CREATE TABLE exam2_2 (
+employee_id  NUMBER,
+bonus_amt   NUMBER DEFAULT 0
+);
+
+SELECT  employee_id, emp_name
+FROM employees
+WHERE department_id IS  NULL
+ORDER BY employee_Id;
+
+SELECT employee_id,salary
+FROM employees
+WHERE salary >=  4500 AND salary <= 5000
+ORDER BY employee_id;
+
+INSERT INTO exam2_2(employee_id)
+SELECT b.employee_id
+FROM employees b , sales s
+WHERE b.employee_id = s.employee_id
+AND s.SALES_MONTH BETWEEN '200010' AND '200013'
+GROUP BY b.employee_id;
+
+SELECT
+    *
+FROM exam2_2;
+
+MERGE INTO exam2_2 d
+USING (SELECT employee_id,salary,manager_id
+            FROM employees
+            WHERE manager_id = 146) b
+            ON (d.employee_id = b.employee_id)
+WHEN MATCHED THEN
+    UPDATE SET d.bonus_amt = d.bonus_amt + b.salary * 0.05
+WHEN NOT MATCHED THEN
+    INSERT (d.employee_id, d.bonus_amt )  VALUES (b.employee_id, b.salary * 0.02);
+    
+SELECT
+    *
+FROM exam2_2;
+
+COMMIT; 
